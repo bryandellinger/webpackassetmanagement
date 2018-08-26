2c0860a9c52811a031c5d2d8d8b085e11ebe9678
Bryan Dellinger <dellingerfam5@gmail.com>
2018-08-26 19:28:40 -0400

    webpackplugin

--- src/index.js
+++ src/index.js
@@ -1,24 +1,20 @@
 import _ from 'lodash';
-import './style.css';
-import Icon from './jasmine_favicon.png';
-import Data from './data.xml';
+import printMe from './print.js'
+
 
 function component() {
     let element = document.createElement('div');
+    var btn = document.createElement('button');
   
     // Lodash, currently included via a script, is required for this line to work
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    element.classList.add('hello'); 
-     // Add the image to our existing div.
-     var myIcon = new Image();
-     myIcon.src = Icon;
 
-     console.log(Data);
+    
+  btn.innerHTML = 'Click me and check the console!';
+  btn.onclick = printMe;
 
-    element.appendChild(myIcon);
+  element.appendChild(btn);
     return element;
   }
-
-
   
   document.body.appendChild(component());
